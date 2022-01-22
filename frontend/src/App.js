b6e1d7c43277138d5fa9117161c2376c5bbdad67
Corey Hellwege <coreyhellwege@gmail.com>
2022-01-22 17:53:13 +1100

    Create Product Screen. Use useParams instead of match.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-            <Route path='/' element={<HomeScreen />} exact />
+            <Route exact path='/' element={<HomeScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
           </Routes>
         </Container>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,64 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, ListGroupItem, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
 const ProductScreen = () => {
-  return <div>Product</div>
+    const { id } = useParams()
+    const product = products.find((p) => p._id === id) // Match product id with params id from the url
+
+    return (
+        <>
+            <Link className='btn btn-light my-3' to='/'>
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Price: ${product.price}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Description: {product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col><strong>{product.price}</strong></Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Button className='w-100' disabled={product.countInStock === 0}>
+                                    Add To Cart
+                                </Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    )
 }
 
 export default ProductScreen
