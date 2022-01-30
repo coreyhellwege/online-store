4fa521883ac5f3d016ebec271d09e3d0f04d610e
Corey Hellwege <coreyhellwege@gmail.com>
2022-01-30 22:42:37 +1100

    Create custom error handler middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -20,7 +20,12 @@ router.get('/', asyncHandler(async (req, res) => {
  */
 router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
-    product ? res.json(product) : res.status(404).json({ message: 'Product not found.' })
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
 }))
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -16,6 +17,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound) // Handle missing route errors.
+app.use(errorHandler) // Override the default error handler.
 
 const PORT = process.env.PORT || 8000
 const ENV = process.env.NODE_ENV
