67961f66da746f88caf8efe78617a2a8e79000e2
Corey Hellwege <coreyhellwege@gmail.com>
2022-02-01 21:22:53 +1100

    Bring Redux state into the home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,36 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-    // First arg 'products' is what you want to call the state.
-    // Second arg 'setProducts' is the function you want to manipulate the state.
-    // useState takes a default value for 'products'.
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
     // useEffect runs as soon as the component loads.
     // Second arg is an array of dependencies which you want to trigger useEffect when they change.
     useEffect(() => {
-        // Use async/await along with Axios when making backend requests.
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        }
-
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
 
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+            {
+                loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : 
+                <Row>
+                    {products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            }
         </>
     )
 }
