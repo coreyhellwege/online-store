e4ef95fa63a009ab985f4221c3bcd73a324b2e6d
Corey Hellwege <coreyhellwege@gmail.com>
2022-02-08 23:24:10 +1100

    Render products added to cart in Cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,16 +1,15 @@
 import React, { useEffect } from 'react';
-import { Link, useParams, useLocation } from 'react-router-dom'
+import { Link, useParams, useLocation, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, ListGroupItem, Card, Button, Form, FormControl } from 'react-bootstrap'
 import Message from '../components/Message'
 import { addToCart } from '../actions/cartActions'
-
-
 
 const CartScreen = () => {
     const { id } = useParams(),
         { search } = useLocation(),
         dispatch = useDispatch(),
+        navigate = useNavigate(),
         qty = new URLSearchParams(search).get('qty'),
         cart = useSelector(state => state.cart),
         { cartItems } = cart
@@ -20,10 +19,55 @@ const CartScreen = () => {
             dispatch(addToCart(id, qty))
         }
     }, [dispatch, id, qty])
+
+    const removeFromCartHandler = (id) => {
+        console.log('remove');
+    }
+
+    const checkoutHandler = () => {
+        navigate('/login?redirect=shipping')
+    }
     
-    return <div>
-        Cart
-    </div>;
+    return (
+        <Row>
+            <Col md={8}>
+                <h1>Shopping Cart</h1>
+                {
+                    cartItems.length === 0 ? <Message>Cart is empty <Link to='/'>Go back</Link></Message> :
+                    <ListGroup variant='flush'>
+                        {cartItems.map(item => (
+                            <ListGroupItem key={item.product}>
+                                <Row>
+                                    <Col md={2}><Image src={item.image} alt={item.name} fluid rounded /></Col>
+                                    <Col md={3}><Link to={`/product/${item.product}`}>{item.name}</Link></Col>
+                                    <Col md={2}>${item.price}</Col>
+                                    <Col md={2}>
+                                        <FormControl as='select' value={item.qty} onChange={e => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                                            {[...Array(item.countInStock).keys()].map(x => <option key={x + 1} value={x + 1}>{x + 1}</option>)}
+                                        </FormControl>
+                                    </Col>
+                                    <Col md={2}><Button type='button' variant='light' onClick={() => removeFromCartHandler(item.product)}><i className='fas fa-trash'></i></Button></Col>
+                                </Row>  
+                            </ListGroupItem>
+                        ))}
+                    </ListGroup>
+                }
+            </Col>
+            <Col md={4}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
+                            ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Button type='button' className='btn-block' disabled={cartItems.length === 0} onClick={checkoutHandler}>Proceed to checkout</Button>
+                        </ListGroupItem>
+                    </ListGroup>
+                </Card>
+            </Col>
+        </Row>
+    )
 };
 
 export default CartScreen;
