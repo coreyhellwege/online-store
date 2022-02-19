ef496053bb0442593eb36626159022b514d04c5b
Corey Hellwege <coreyhellwege@gmail.com>
2022-02-19 15:58:46 +1100

    Create register user route and controller, plus middleware to encrypt the password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -25,11 +25,23 @@ const userSchema = mongoose.Schema({
 })
 
 /*
- * Compares an entered password to the stored hashed password for a user using bcrypt.
+ * (Middleware) Compares an entered password to the stored hashed password for a user using bcrypt.
  */
 userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
+
+/*
+ * (Middleware) If the user's password is set or modified, encrypt it before saving the model.
+ */
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+    } else {
+        next()
+    }
+})
 
 const User = mongoose.model('User', userSchema) // create a model from the schema
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import { registerUser, authUser, getUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
+router.post('/', registerUser)
 router.post('/login', authUser)
 router.get('/profile', protect, getUserProfile)
 
