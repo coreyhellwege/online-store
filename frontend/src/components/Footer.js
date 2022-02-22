b9ac683b28201a357fee91acbb785d2a16e688eb
Corey Hellwege <coreyhellwege@gmail.com>
2022-02-22 22:50:09 +1100

    Tidy up components

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,15 +2,9 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className='text-center py-3'>Copyright &copy; Online Store</Col>
-                </Row>
-            </Container>
-        </footer>
-    )
+    return <footer>
+        <Container><Row><Col className='text-center py-3'>Copyright &copy; Online Store</Col></Row></Container>
+    </footer>
 }
 
 export default Footer
--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -2,71 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Rating = ({ value, text, color }) => {
-  return (
-    <div className='rating'>
-      <span>
-        <i
-          style={{ color }}
-          className={
-            value >= 1
-              ? 'fas fa-star'
-              : value >= 0.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{ color }}
-          className={
-            value >= 2
-              ? 'fas fa-star'
-              : value >= 1.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{ color }}
-          className={
-            value >= 3
-              ? 'fas fa-star'
-              : value >= 2.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{ color }}
-          className={
-            value >= 4
-              ? 'fas fa-star'
-              : value >= 3.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{ color }}
-          className={
-            value >= 5
-              ? 'fas fa-star'
-              : value >= 4.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>{text ?? text}</span>
-    </div>
-  )
+    return (
+        <div className='rating'>
+            <span><i style={{ color }} className={value >= 1 ? 'fas fa-star' : value >= 0.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i></span>
+            <span><i style={{ color }} className={value >= 2 ? 'fas fa-star' : value >= 1.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i></span>
+            <span><i style={{ color }} className={value >= 3 ? 'fas fa-star' : value >= 2.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i></span>
+            <span><i style={{ color }} className={value >= 4 ? 'fas fa-star' : value >= 3.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i></span>
+            <span><i style={{ color }} className={value >= 5 ? 'fas fa-star' : value >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'}></i></span>
+            <span>{text ?? text}</span>
+        </div>
+    )
 }
 
 Rating.defaultProps = {
@@ -76,7 +21,7 @@ Rating.defaultProps = {
 
 Rating.propTypes = {
   value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
+  text : PropTypes.string.isRequired,
   color: PropTypes.string
 }
 
