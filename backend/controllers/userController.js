8f21eeda47c44b9dbd2701af1b3c119612881a0e
Corey Hellwege <coreyhellwege@gmail.com>
2022-02-24 21:36:28 +1100

    Create updateUserProfile controller function and PUT route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,6 +48,37 @@ const getUserProfile = asyncHandler(async (req, res) => {
 })
 
 /*
+ * @desc   Update user profile.
+ * @route  PUT /api/users/profile
+ * @access Private
+ */
+const upateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id) 
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token : generateToken(updatedUser._id)
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+/*
  * @desc   Register a new user.
  * @route  POST /api/users
  * @access Public
@@ -81,4 +112,4 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile, registerUser }
+export { authUser, getUserProfile, registerUser, upateUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { registerUser, authUser, getUserProfile } from '../controllers/userController.js'
+import { registerUser, authUser, getUserProfile, upateUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/', registerUser)
 router.post('/login', authUser)
-router.get('/profile', protect, getUserProfile)
+router.get('/profile', protect, getUserProfile).put('/profile', protect, upateUserProfile)
 
 export default router
