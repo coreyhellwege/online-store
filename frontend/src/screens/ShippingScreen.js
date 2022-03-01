31d4bb3f657f0cf4f68a0369dabf7a0e98666653
Corey Hellwege <coreyhellwege@gmail.com>
2022-03-01 21:14:02 +1100

    Create CheckoutSteps component and rener in shipping screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = ({ history }) => { // destructure props and get history
@@ -20,7 +21,9 @@ const ShippingScreen = ({ history }) => { // destructure props and get history
         navigate('/payment') // then navigate to the payment page
     }
     
+    // Pass the current and previous steps to CheckoutSteps
     return <FormContainer>
+        <CheckoutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
             <Form.Group controlId='address'>
