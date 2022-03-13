4370e1e9e5a9e67e3b611c55bfa4ecb695450c75
Corey Hellwege <coreyhellwege@gmail.com>
2022-03-13 18:05:24 +1100

    Add some validation to prevent the user from manipulating item prices from local storage

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
+import Product from '../models/productModel.js'
 
 /*
  * @desc   Create a new order.
@@ -14,6 +15,18 @@ const addOrderItems = asyncHandler(async (req, res) => {
         throw new Error('No order items')
         return
     } else {
+        // Add some validation to prevent the user from manipulating item prices from local storage
+        orderItems.forEach(async (item) => {
+            console.log(item)
+            let lookupItem = await Product.findById(item.product)
+            console.log(lookupItem)
+            if (item.price !== lookupItem.price) {
+                res.status(400)
+                throw new Error('Item prices don\'t match, please try again')
+                return
+            }
+        })
+
         const order = new Order({
             user: req.user._id, // get user id from the token as this is a protected route
             orderItems, 
