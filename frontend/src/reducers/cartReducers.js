c8d0d0b52300e4723e1baa379dc863f1ad7de34f
Corey Hellwege <coreyhellwege@gmail.com>
2022-03-14 17:43:56 +1100

    Create order pay reducer and action

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -7,31 +7,16 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                   itemExists = state.cartItems.find(i => i.product === item.product)
 
             if (itemExists) {
-                return {
-                    ...state,
-                    cartItems: state.cartItems.map(i => i.product === itemExists.product ? item : i)
-                }
+                return { ...state, cartItems: state.cartItems.map(i => i.product === itemExists.product ? item : i) }
             } else {
-                return {
-                    ...state,
-                    cartItems: [...state.cartItems, item]
-                }
+                return { ...state, cartItems: [...state.cartItems, item] }
             }
         case CART_REMOVE_ITEM:
-            return {
-                ...state,
-                cartItems: state.cartItems.filter(i => i.product !== action.payload)
-            }
+            return { ...state, cartItems: state.cartItems.filter(i => i.product !== action.payload) }
         case CART_SAVE_SHIPPING_ADDRESS:
-            return {
-                ...state,
-                shippingAddress: action.payload
-            }
+            return { ...state, shippingAddress: action.payload }
         case CART_SAVE_PAYMENT_METHOD:
-            return {
-                ...state,
-                paymentMethod: action.payload
-            }
+            return { ...state, paymentMethod: action.payload }
         default:
             return state
     }
