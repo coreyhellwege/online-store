bc9c9928f46816c7ab58ba28461e82bae134f044
Corey Hellwege <coreyhellwege@gmail.com>
2022-04-20 21:47:06 +0930

    Create route and controller method for getting the logged-in user's orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderById, updateOrderToPaid } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/', protect, addOrderItems)
+router.get('/myorders', protect, getMyOrders)
 router.get('/:id', protect, getOrderById)
 router.put('/:id/pay', protect, updateOrderToPaid)
 
