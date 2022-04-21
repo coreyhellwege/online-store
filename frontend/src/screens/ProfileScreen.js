a24e1a65e4a17112741889c1be6f49f8a45c5874
Corey Hellwege <coreyhellwege@gmail.com>
2022-04-21 20:23:14 +1000

    Dispatch the listMyOrders action. Render a table with the orders' details.

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Table, Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import { listMyOrders } from '../actions/orderActions'
 
 const ProfileScreen = () => {
     const dispatch = useDispatch(), navigate = useNavigate()
@@ -18,14 +20,15 @@ const ProfileScreen = () => {
     const userDetails = useSelector(state => state.userDetails), { loading, error, user } = userDetails
     const userLogin = useSelector(state => state.userLogin), { userInfo } = userLogin 
     const userUpdateProfile = useSelector(state => state.userUpdateProfile), { success } = userUpdateProfile
+    const myOrders = useSelector(state => state.myOrders), { loading: loadingOrders, error: errorOrders, orders } = myOrders
 
     useEffect(() => {
         if (!userInfo) {
             navigate('/login') // if user is not logged-in, redirect them to log in
         } else {
             if (!user.name) {
-                // if there's no user name, pass 'profile' to getUserDetails (instead of an id) to get the logged-in user
-                dispatch(getUserDetails('profile'))
+                dispatch(getUserDetails('profile')) // if there's no user name, pass 'profile' to getUserDetails (instead of an id) to get the logged-in user
+                dispatch(listMyOrders())
             } else {
                 setName(user.name)
                 setEmail(user.email)
@@ -67,6 +70,36 @@ const ProfileScreen = () => {
         </Col>
         <Col md={9}>
             <h2>My Orders</h2>
+            {loadingOrders ? <Loader /> : errorOrders ? <Message variant='danger'>{errorOrders}</Message> : (
+                <Table striped bordered hover responsive className='table-sm'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Date</th>
+                            <th>Total</th>
+                            <th>Paid</th>
+                            <th>Delivered</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.map((order) => (
+                            <tr key={order._id}>
+                                <td>{order._id}</td>
+                                <td>{order.createdAt.substring(0,10)}</td> 
+                                <td>{order.totalPrice}</td>
+                                <td>{order.isPaid ? order.paidAt.substring(0,10) : <i className='fas fa-times' style={{color: 'red'}}></i>}</td>
+                                <td>{order.isDelivered ? order.deliveredAt.substring(0,10) : <i className='fas fa-times' style={{color: 'red'}}></i>}</td>
+                                <td>
+                                    <LinkContainer to={`/order/${order._id}`}>
+                                        <Button className='btn-sm' variant='light'>Details</Button>
+                                    </LinkContainer>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
         </Col>
     </Row>
 }
