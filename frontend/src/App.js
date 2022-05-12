12665943a1da4d6ed8fbd05d45edd45d25b7cdfe
Corey Hellwege <coreyhellwege@gmail.com>
2022-05-12 20:31:12 +1000

    Render list of users and links to the Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
 
 const App = () => {
     // Must return one element
@@ -21,17 +22,18 @@ const App = () => {
             <main className='py-3'>
                 <Container>
                     <Routes>
+                        <Route exact path='/' element={<HomeScreen />} />
                         <Route path='/login' element={<LoginScreen />} />
                         <Route path='/register' element={<RegisterScreen />} />
                         <Route path='/profile' element={<ProfileScreen />} />
                         <Route path='/product/:id' element={<ProductScreen />} />
                         <Route path='/cart/:id' element={<CartScreen />} />
-                        <Route path="/cart/" element={<CartScreen />} />
-                        <Route path="/shipping/" element={<ShippingScreen />} />
-                        <Route path="/payment/" element={<PaymentScreen />} />
-                        <Route path="/placeorder/" element={<PlaceOrderScreen />} />
+                        <Route path="/cart" element={<CartScreen />} />
+                        <Route path="/shipping" element={<ShippingScreen />} />
+                        <Route path="/payment" element={<PaymentScreen />} />
+                        <Route path="/placeorder" element={<PlaceOrderScreen />} />
                         <Route path="/order/:id" element={<OrderScreen />} />
-                        <Route exact path='/' element={<HomeScreen />} />
+                        <Route path="/admin/userlist" element={<UserListScreen />} />
                     </Routes>
                 </Container>
             </main>
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -13,26 +13,51 @@ const UserListScreen = () => {
     useEffect(() => {
       dispatch(listUsers()) // Dispatch action to retrieve the users data
     }, [dispatch])
+
+    const deleteHandler = (id) => {
+        console.log('delete')
+    }
     
-  return <>
-    <h1>Users</h1>
-    {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-        <Table striped bordered hover responsive className='table-sm'>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Admin</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                
-            </tbody>
-        </Table>
-    )}
-  </>
+    return <>
+        <h1>Users</h1>
+        {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+            <Table striped bordered hover responsive className='table-sm'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Admin</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => (
+                        <tr key={user._id}>
+                            <td>{user._id}</td>
+                            <td>{user.name}</td>
+                            <td><a href={`mailto:${user.email}`}>{user.email}</a></td>
+                            <td>
+                                {user.isAdmin ? 
+                                    (<i className="fa fa-check" style={{ color: 'green' }} />) :
+                                    (<i className="fa fa-times" style={{ color: 'red' }} />)}
+                            </td>
+                            <td>
+                                <LinkContainer to={`./user/${user._id}/edit`}>
+                                    <Button variant='light' className='btn-sm'>
+                                        <i className='fas fa-edit' />
+                                    </Button>
+                                </LinkContainer>
+                                <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(user._id)}>
+                                    <i className='fas fa-trash' />
+                                </Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        )}
+    </>
 }
 
 export default UserListScreen
