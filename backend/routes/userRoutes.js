1fd11f03e24333a64ff1149d87079a13e36c3246
Corey Hellwege <coreyhellwege@gmail.com>
2022-05-23 12:17:07 +1000

    Create deleteUser controller method and backend route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { registerUser, authUser, getUserProfile, upateUserProfile, getUsers } from '../controllers/userController.js'
+import { registerUser, authUser, getUserProfile, upateUserProfile, getUsers, deleteUser } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.post('/', registerUser).get('/', protect, admin, getUsers)
 router.post('/login', authUser)
 router.get('/profile', protect, getUserProfile).put('/profile', protect, upateUserProfile)
+router.delete('/:id', protect, admin, deleteUser)
 
 export default router
