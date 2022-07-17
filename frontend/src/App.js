6b472fccb3d044c6c3bb2621cda7ff6eeefa7309
Corey Hellwege <coreyhellwege@gmail.com>
2022-07-17 14:16:25 +1000

    Create product list screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 const App = () => {
     // Must return one element
@@ -36,6 +37,7 @@ const App = () => {
                         <Route path="/order/:id" element={<OrderScreen />} />
                         <Route path="/admin/userlist" element={<UserListScreen />} />
                         <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+                        <Route path="/admin/productlist" element={<ProductListScreen />} />
                     </Routes>
                 </Container>
             </main>
---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,83 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Table, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProducts } from '../actions/productActions'
+
+const ProductListScreen = () => {
+    const dispatch = useDispatch(), navigate = useNavigate()
+    const productList = useSelector(state => state.productList), { loading, error, products } = productList // Get list of users from the global state
+    const userLogin = useSelector(state => state.userLogin), { userInfo } = userLogin
+
+    useEffect(() => {
+        if (userInfo && userInfo.isAdmin) {
+            dispatch(listProducts())
+        } else {
+            navigate('/login')
+        }
+    }, [dispatch, userInfo, navigate])
+
+    const deleteHandler = (user) => {
+        if (window.confirm(`Are you sure you want to delete ${user.name}?`)) {
+            // delete products
+        }
+    }
+
+    const createProductHandler = (product) => {
+        // create product
+    }
+    
+    return <>
+        <Row className='align-items-center'>
+            <Col>
+                <h1>Products</h1>
+            </Col>
+            <Col className='text-end'>
+                <Button className='my-3' onClick={createProductHandler}>
+                    <i className='fa fa-plus'></i>
+                    &nbsp;Create Product
+                </Button>
+            </Col>
+        </Row>
+        {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+            <Table striped bordered hover responsive className='table-sm'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Category</th>
+                        <th>Brand</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr key={product._id}>
+                            <td>{product._id}</td>
+                            <td>{product.name}</td>
+                            <td>${product.price}</td>
+                            <td>{product.category}</td>
+                            <td>{product.brand}</td>
+                            <td>
+                                <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                                    <Button variant='light' className='btn-sm'>
+                                        <i className='fas fa-edit' />
+                                    </Button>
+                                </LinkContainer>
+                                <Button variant='danger' className='btn-sm' onClick={() => deleteHandler(product._id)}>
+                                    <i className='fas fa-trash' />
+                                </Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        )}
+    </>
+}
+
+export default ProductListScreen
