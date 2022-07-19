f8111a0e5c9e3744d7f1fd0413db3fc84a89a743
Corey Hellwege <coreyhellwege@gmail.com>
2022-07-19 22:08:53 +1000

    Return current state when PRODUCT_DETAILS_REQUEST action runs

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -8,7 +8,7 @@ import {
 export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
-            return { loading: true, ...state }
+            return { loading: true }
         case PRODUCT_LIST_SUCCESS:
             return { loading: false, products: action.payload }
         case PRODUCT_LIST_FAIL:
@@ -21,7 +21,7 @@ export const productListReducer = (state = { products: [] }, action) => {
 export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
     switch (action.type) {
         case PRODUCT_DETAILS_REQUEST:
-            return { loading: true, products: [] }
+            return { ...state, loading: true }
         case PRODUCT_DETAILS_SUCCESS:
             return { loading: false, product: action.payload }
         case PRODUCT_DETAILS_FAIL:
