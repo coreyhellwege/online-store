0e7c9380efc0bc3f867e53234b6e223f91010d47
Corey Hellwege <coreyhellwege@gmail.com>
2022-11-01 20:28:06 +1100

    Create image validation middleware

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,33 @@
+import path from 'path'
+import multer from 'multer'
+
+// Initialise the multer storage engine
+const storage = multer.diskStorage({
+    destination(req, file, callback) {
+        callback(null, '/uploads')
+    },
+    filename(req, file, callback) {
+        callback(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const checkFileType = (file, callback) => {
+    const filetypes = /jpg|jpeg|png/ // allowed extensions
+    const validExtension = filetypes.test(path.extname(file.originalname).toLowerCase()) // check extension
+    const validMimetype = filetypes.test(file.mimetype) // check mimetype
+
+    if (validExtension && validMimetype) {
+        return callback(null, true)
+    } else {
+        callback(new Error('Only .png, .jpg and .jpeg formats allowed'))
+    }
+}
+
+const uploadImages = multer({
+    storage,
+    fileFilter: (req, file, callback) => {
+        checkFileType(file, callback)
+    }
+})
+
+export { uploadImages }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import path from 'path'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
@@ -8,6 +9,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -26,8 +28,15 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+// The __dirname Nodejs environment variable is only compatible with commonJS, not ES modules, so just mimick it using the path module
+const __dirname = path.resolve()
+
+// Use Express to make the uploads folder static so it can be loaded in the browser
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound) // Handle missing route errors.
 app.use(errorHandler) // Override the default error handler.
