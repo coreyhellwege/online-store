c4743ff33f34489a44e7f31fc1112e86a932cb9c
Corey Hellwege <coreyhellwege@gmail.com>
2022-11-13 22:13:42 +1100

    Create createProductReview controller method and POST route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js'
+import { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createProductReview } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.get('/', getProducts).post('/', protect, admin, createProduct)
 router.get('/:id', getProductById).delete('/:id', protect, admin, deleteProduct).put('/:id', protect, admin, updateProduct)
+router.post('/:id/reviews', protect, createProductReview)
 
 export default router
