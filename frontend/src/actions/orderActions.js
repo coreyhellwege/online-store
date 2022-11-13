69af8c9be82d3990a4a4e8e44314d416bdd4dfb7
Corey Hellwege <coreyhellwege@gmail.com>
2022-11-13 20:10:27 +1100

    Create deliverOrder action and constants

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -4,7 +4,8 @@ import {
     ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL,
     ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL,
     MY_ORDERS_REQUEST, MY_ORDERS_SUCCESS, MY_ORDERS_FAIL, 
-    ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL
+    ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL, 
+    ORDER_DELIVER_REQUEST, ORDER_DELIVER_SUCCESS, ORDER_DELIVER_FAIL
 } from '../constants/orderConstants'
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -61,6 +62,21 @@ export const payOrder = (id, paymentResult) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: ORDER_PAY_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const deliverOrder = (order) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: ORDER_DELIVER_REQUEST })
+        const { userLogin: { userInfo } } = getState() // destructure userInfo from getState (two levels in)
+        const config = { headers: { 'Authorization': `Bearer ${userInfo.token}` } }
+        const { data } = await axios.put(`/api/orders/${order._id}/deliver`, {}, config)
+        dispatch({ type: ORDER_DELIVER_SUCCESS, payload: data })
+    } catch (error) {
+        dispatch({
+            type: ORDER_DELIVER_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
