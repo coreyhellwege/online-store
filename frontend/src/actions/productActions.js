c203aff9ee070cbe88907cc0c31f7061b1751f8a
Corey Hellwege <coreyhellwege@gmail.com>
2022-11-15 21:36:10 +1100

    Create product review create action, reducer and constants

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,7 +4,8 @@ import {
     PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, 
     PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAIL,
     PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL,
-    PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL
+    PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, 
+    PRODUCT_CREATE_REVIEW_REQUEST, PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_FAIL
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
@@ -84,6 +85,26 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_DELETE_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const createProductReview = (id, review) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: PRODUCT_CREATE_REVIEW_REQUEST })
+        const { userLogin: { userInfo } } = getState() // destructure userInfo from getState (two levels in)
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userInfo.token}`
+            }
+        }
+        axios.post(`/api/products/${id}/reviews`, review, config)
+        dispatch({ type: PRODUCT_CREATE_REVIEW_SUCCESS })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_CREATE_REVIEW_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,31 +3,32 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 // Import reducer functions
-import { productListReducer, productDetailsReducer, productCreateReducer, productUpdateReducer, productDeleteReducer } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer, productCreateReducer, productUpdateReducer, productDeleteReducer, productReviewCreateReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer, userListReducer, userDeleteReducer, userUpdateReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderDeliverReducer, listMyOrdersReducer, orderListReducer } from './reducers/orderReducers'
 
 const rootReducer = combineReducers({
-    productList      : productListReducer,
-    productDetails   : productDetailsReducer,
-    productCreate    : productCreateReducer,
-    productUpdate    : productUpdateReducer,
-    productDelete    : productDeleteReducer,
-    cart             : cartReducer,
-    userLogin        : userLoginReducer,
-    userRegister     : userRegisterReducer,
-    userDetails      : userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer,
-    userList         : userListReducer,
-    userDelete       : userDeleteReducer,
-    userUpdate       : userUpdateReducer,
-    orderCreate      : orderCreateReducer,
-    orderDetails     : orderDetailsReducer,
-    orderPay         : orderPayReducer,
-    orderDeliver     : orderDeliverReducer,
-    myOrders         : listMyOrdersReducer,
-    orderList        : orderListReducer
+    productList        : productListReducer,
+    productDetails     : productDetailsReducer,
+    productCreate      : productCreateReducer,
+    productUpdate      : productUpdateReducer,
+    productDelete      : productDeleteReducer,
+    productReviewCreate: productReviewCreateReducer,
+    cart               : cartReducer,
+    userLogin          : userLoginReducer,
+    userRegister       : userRegisterReducer,
+    userDetails        : userDetailsReducer,
+    userUpdateProfile  : userUpdateProfileReducer,
+    userList           : userListReducer,
+    userDelete         : userDeleteReducer,
+    userUpdate         : userUpdateReducer,
+    orderCreate        : orderCreateReducer,
+    orderDetails       : orderDetailsReducer,
+    orderPay           : orderPayReducer,
+    orderDeliver       : orderDeliverReducer,
+    myOrders           : listMyOrdersReducer,
+    orderList          : orderListReducer
 })
 
 const cartItemsFromStorage       = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
