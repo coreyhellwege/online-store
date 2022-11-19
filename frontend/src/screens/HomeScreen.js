556e853997b747fee3b322ad0795c3fa8f427e98
Corey Hellwege <coreyhellwege@gmail.com>
2022-11-19 18:51:27 +1100

    Add the Paginate component to the Home Screen. Pass it the search keyword from the frontend route, and 'pages' and 'page' from the productList global state.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,26 +6,32 @@ import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
 
 const HomeScreen = () => {
     const params = useParams(), dispatch = useDispatch()
-    const productList = useSelector(state => state.productList), { loading, error, products } = productList
+    const keyword = params.keyword,
+          pageNumber = params.pageNumber || 1
+    const productList = useSelector(state => state.productList), { loading, error, products, page, pages } = productList
 
     useEffect(() => {
-        dispatch(listProducts(params.keyword))
-    }, [dispatch, params.keyword])
+        dispatch(listProducts(keyword, pageNumber))
+    }, [dispatch, keyword, pageNumber])
 
     return <>
         <h1>Latest Products</h1>
         {
             loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : 
-            <Row>
-                {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+            <>
+                <Row>
+                    {products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+                <Paginate pages={pages} page={page} keyword={keyword ? keyword : ''} />
+            </>
         }
     </>
 }
